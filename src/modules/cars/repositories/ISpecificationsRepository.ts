1d738333c42842a85d99272760e35af933faaee8
Jefferson <jeffersonsantos.a@gmail.com>
2021-08-30 11:56:54 -0300

    Criando migration de especificaçao

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -6,9 +6,9 @@ interface ICreateSpecificationDTO {
 }
 
 interface ISpecificationsRepository {
-  create({ description, name }: ICreateSpecificationDTO): void;
+  create({ description, name }: ICreateSpecificationDTO): Promise<void>;
 
-  findByName(name: string): Specification;
+  findByName(name: string): Promise<Specification>;
 }
 
 export { ICreateSpecificationDTO, ISpecificationsRepository };
--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -4,29 +4,29 @@ import { ICategoriesRepository, ICreateCategoryDTO } from "./../ICategoriesRepos
 
 class CategoriesRepository implements ICategoriesRepository {
 
-  private repository: Repository<Category>
+  private repository: Repository<Category>;
 
   constructor() {
-    this.repository = getRepository(Category)
+    this.repository = getRepository(Category);
   }
 
   async create({ name, description }: ICreateCategoryDTO): Promise<void> {
     const category = this.repository.create({
       description,
       name
-    })
+    });
 
-    await this.repository.save(category)
+    await this.repository.save(category);
   }
 
   async list(): Promise<Category[]> {
-    const categories = await this.repository.find()
+    const categories = await this.repository.find();
 
-    return categories
+    return categories;
   }
 
   async findByName(name: string): Promise<Category> {
-    const category = this.repository.findOne({ name })
+    const category = this.repository.findOne({ name });
 
     return category;
   }
