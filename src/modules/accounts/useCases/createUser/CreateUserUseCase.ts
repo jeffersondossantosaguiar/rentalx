892da0308835ba807fdd9451f9564a3a18a0c08a
Jefferson <jeffersonsantos.a@gmail.com>
2021-08-30 15:23:18 -0300

    Criando repositorio de usuario

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,26 @@
+import { inject } from "tsyringe";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUserRepository";
+
+class CreateUserUseCase {
+
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {
+  }
+
+  async execute({ name, username, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+
+    await this.usersRepository.create({
+      name,
+      username,
+      email,
+      driver_license,
+      password
+    })
+  }
+
+}
+
+export { CreateUserUseCase };
