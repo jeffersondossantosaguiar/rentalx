54555cc13c164d9b66e74a137abf3b351eece2ae
Jefferson <jeffersonsantos.a@gmail.com>
2021-08-31 15:44:28 -0300

    Criptografar senha

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt';
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUserRepository";
@@ -13,14 +14,22 @@ class CreateUserUseCase {
 
   async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
 
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (userAlreadyExists)
+      throw new Error("User already exists")
+
+    const passwordHash = await hash(password, 8)
+
     await this.usersRepository.create({
       name,
       email,
       driver_license,
-      password
+      password: passwordHash
     })
   }
 
 }
 
 export { CreateUserUseCase };
+
